727e2561eddfde522d5a7e61a827721a124b9fc1
fabioQfabio <a78793@alunos.uminho.pt>
2023-02-03 14:06:14 +0000

    added some work to part 1 and some doc

--- challange/src/main/scala/Challenges.scala
+++ challange/src/main/scala/Challenges.scala
@@ -1,6 +1,7 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.desc
+//import org.apache.spark.sql.functions.desc
+import org.apache.spark.sql.functions._
 
 object Challenges extends App {
 
@@ -23,9 +24,29 @@ object Challenges extends App {
   val df_store = spark.read.option("header", true).csv(filePathStore)
 
 
+  //-------------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------
+
+  //df.show()
+
+  /** Part 1
+   * Filters only rows with valid "Sentiment_Polarity" values
+   * Groups by App name, calculating de average of "Sentiment_Polarity", renaming it "Average_Sentiment_Polarity".
+   */
+  val df_1 = df.where(col("Sentiment_Polarity").cast("Double").isNotNull && isnan(col("Sentiment_Polarity") ) === false)
+                .groupBy("App").agg(mean("Sentiment_Polarity").alias("Average_Sentiment_Polarity"))
+                .sort("App")
+
+  //df_1.show()
+
+  //-------------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------------
+
 
   //df_store.show()
-  /**
+  /** Part 2
    * Selects both "App" and "Rating" columns, however, only when the rating is between 4.0 and 5.0.
    * Orders by "Rating" starting from 5.0 and going down .
    */
@@ -34,7 +55,10 @@ object Challenges extends App {
                               .orderBy(desc("Rating"))
   //df_best_apps.show()
 
-
+  /**
+   * Writes in a csv file, puts the header in the first line, and sets delimiter "$"
+   * (use coalesce(1) to only write in 1 fragment)
+   */
   //df_best_apps.write.options(Map("header"->"true", "delimiter"->"$")).csv("src/main/resources/best_apps.csv")
   //df_best_apps.coalesce(1).write.options(Map("header"->"true", "delimiter"->"$")).csv("src/main/resources/best_apps.csv")
 
